0e9524a3812cf27728c9d11080c8e148b8c5c4a8
walnut-tree-387 <rahamanmohammadataur@gmail.com>
2026-01-09 11:55:36 +0600

    feat : GlobalExceptionHandler registered

---
+++ src/main/java/com/sisimpur/library/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.sisimpur.library.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
