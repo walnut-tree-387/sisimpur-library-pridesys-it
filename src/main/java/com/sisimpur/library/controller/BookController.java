0bd67e95fd8118b74d5a7c58f61fccec1015657d
walnut-tree-387 <rahamanmohammadataur@gmail.com>
2026-01-09 18:43:15 +0600

    feat: exposed book module api

--- src/main/java/com/sisimpur/library/controller/BookController.java
+++ src/main/java/com/sisimpur/library/controller/BookController.java
@@ -1,24 +1,44 @@
 package com.sisimpur.library.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sisimpur.library.dto.books.BookCreateDto;
+import com.sisimpur.library.dto.books.BookGetDto;
+import com.sisimpur.library.dto.books.BookUpdateDto;
+import com.sisimpur.library.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.sisimpur.library.model.Book;
-import com.sisimpur.library.service.BookService;
+import com.sisimpur.library.service.BookServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/books")
-@RequiredArgsConstructor
 public class BookController {
     
     private final BookService bookService;
 
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/{id}")
-    public Book getBook(@PathVariable Long id) {
-        return bookService.getBook(id);
+    public ResponseEntity<BookGetDto> getBook(@PathVariable Long id) {
+        return new ResponseEntity<>(bookService.getBookById(id), HttpStatus.OK);
+    }
+    @PostMapping()
+    public ResponseEntity<BookGetDto> createBook(@RequestBody BookCreateDto createBookDto) {
+        return new ResponseEntity<>(bookService.create(createBookDto), HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.deleteBookById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<BookGetDto> updateBook(@PathVariable Long id, @RequestBody BookUpdateDto updateDto){
+        bookService.updateBookById(id, updateDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
